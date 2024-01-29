b77009a943c5393d10ff5fc54cdef2788d2262e9
NeroRecursif <estezet.n@gmail.com>
2024-01-29 18:53:31 +0100

    Normalize all the line endings

---
+++ web/src/App.tsx
@@ -0,0 +1,8 @@
+import './styles/index.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export const App: React.FC = () => {
+  return <div>Je suis la page de base mec, va falloir booser maintenant !</div>
+}
