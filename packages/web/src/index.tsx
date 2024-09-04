68f65588379142a4b785ba4e4e813bda0025efdb
Nestezet <Nerostalgeek@users.noreply.github.com>
2024-09-04 12:24:02 +0200

    Update index.tsx

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -2,13 +2,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './styles/index.css'; // Your Tailwind CSS file
+import './styles/index.css';
 import { setApiBaseUrl } from '@communest/shared';
 setApiBaseUrl(import.meta.env.VITE_API_BASE_URL);
 
 console.log('env => ', import.meta.env.VITE_API_BASE_URL);
 const container = document.getElementById('root')!;
-const root = createRoot(container); // createRoot instead of ReactDOM.render
+const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <App />
