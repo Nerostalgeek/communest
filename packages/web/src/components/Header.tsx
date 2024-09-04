7c8f0babedf493f22374c02b24e7103a724bf743
Nestezet <Nerostalgeek@users.noreply.github.com>
2024-09-04 12:27:30 +0200

    Update Header.tsx

--- packages/web/src/components/Header.tsx
+++ packages/web/src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { useTheme } from '../App'; // Update this path to the actual location of your App component or where useTheme is defined
+import { useTheme } from '../App';
 import CommunestLogo from './icons/CommunestLogo';
 
 const Header: React.FC = () => {
@@ -11,17 +11,17 @@ const Header: React.FC = () => {
       style={{
         backgroundColor: darkMode
           ? theme.colors.background.dark
-          : theme.colors.background.light, // Assuming these are your theme's dark and light background colors
+          : theme.colors.background.light,
         color: darkMode
           ? theme.colors.primary.dark
-          : theme.colors.primary.light, // Assuming these are your theme's dark and light text colors
+          : theme.colors.primary.light,
         boxShadow: theme.shadows.default,
-        transition: theme.transitions.default, // Smooth transition for theme change
+        transition: theme.transitions.default,
       }}
       className="shadow-md"
     >
       <div className="container mx-auto flex justify-between items-center py-4 px-6">
-        {/* Logo and Title */}
+
         <div className="flex items-center">
           <div className="h-12 mr-3">
             <CommunestLogo className="logo" />
@@ -29,7 +29,7 @@ const Header: React.FC = () => {
           <h1 className="text-xl font-bold">Communest</h1>
         </div>
 
-        {/* Navigation and Mode Toggle */}
+
         <div className="flex items-center">
           <nav>
             <ul className="flex space-x-6 mr-6">
@@ -54,9 +54,9 @@ const Header: React.FC = () => {
           <button
             onClick={toggleDarkMode}
             style={{
-              backgroundColor: '#FFB562', // Assuming this is your secondary color
-              color: '#F5F5F5', // Assuming this is your light background color, used here for contrast
-              borderRadius: '8px', // Example of using a theme-based style
+              backgroundColor: '#FFB562',
+              color: '#F5F5F5',
+              borderRadius: '8px',
             }}
             className="px-4 py-2 rounded"
           >
